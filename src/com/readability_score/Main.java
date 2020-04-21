90c4b10908d5470ea0313807953c7b1750468b5c
yura_borovoy <ysborovoy@gmail.com>
2020-04-21 08:26:06 +0300

    The program counts quantity of words in a sentence and finds average quantity of words in sentences

--- src/com/readability_score/Main.java
+++ src/com/readability_score/Main.java
@@ -1,13 +1,25 @@
 package com.readability_score;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String text = scan.nextLine();
-        if (text.length() <=100) {
+        String[] text = scan.nextLine().split("[!?.]");
+        int countOfWords;
+        ArrayList<Integer> wordsInSentence = new ArrayList<>();
+        for (String sentence : text) {
+            String[] array = sentence.split(" ");
+            countOfWords = array.length;
+            wordsInSentence.add(countOfWords);
+        }
+        int difficultyOfText = 0;
+        for (Integer integer : wordsInSentence) {
+            difficultyOfText += integer;
+        }
+
+        if (difficultyOfText / wordsInSentence.size() <=10) {
             System.out.println("EASY");
         } else {
             System.out.println("HARD");
